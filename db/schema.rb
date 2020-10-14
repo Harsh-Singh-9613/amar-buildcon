43e979cc89a8672deadd052d08b1d976506c6622
Harsh <harshsingh9613@gmail.com>
2020-10-14 18:29:16 +0530

    Code Fixes and added add trip feature and its view part

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_083100) do
+ActiveRecord::Schema.define(version: 2020_10_11_095805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,21 @@ ActiveRecord::Schema.define(version: 2020_10_11_083100) do
     t.integer "excavation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trip_records", force: :cascade do |t|
+    t.date "trip_date"
+    t.integer "trip_count"
+    t.integer "distance"
+    t.integer "revenue"
+    t.integer "truck_capacity"
+    t.integer "diesel_quatity"
+    t.integer "diesel_price"
+    t.string "chainage"
+    t.bigint "site_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["site_id"], name: "index_trip_records_on_site_id"
   end
 
   create_table "users", force: :cascade do |t|
