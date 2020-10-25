db4ad630caac2ca5fc8c8564ac112b7db06b7eb2
harshdeeep <harshsingh9613@gmail.com>
2020-10-25 16:51:14 +0530

    Code Fixes of validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,9 +2,8 @@ class Employee < ActiveRecord::Base
 
   has_many :attendances
 
-  validates_presence_of :name, :license_number, :adhaar_number, :mobile_number, :salary, :designation
+  validates_presence_of :name, :license_number, :mobile_number, :salary, :designation
 
-  validates :adhaar_number, length: { is: 12 }
   validates :mobile_number, length: { is: 10 }
   
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,5 @@
 class Site < ActiveRecord::Base
   has_many :trip_records
 
-  validates_presence_of :name, :default_truck_capacity, :lnt_basic_charge, :lnt_leading_charge, :excavation_charges
+  # validates_presence_of :name, :default_truck_capacity, :lnt_basic_charge, :lnt_leading_charge, :excavation_charges
 end
